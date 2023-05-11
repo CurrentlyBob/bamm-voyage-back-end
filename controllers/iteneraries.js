0b9f14e55eac66a83b381b4e5b9364be51447ef0
CurrentlyBobe <walshbobby5@gmail.com>
2023-05-11 17:28:05 -0500

    add  initial controllers

---
+++ controllers/iteneraries.js
@@ -0,0 +1,69 @@
+import { Profile } from "../models/profile.js";
+import { Itinerary } from "../models/itinerary.js";
+
+
+async function create(req, res) {
+    try {
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function index(req, res) {
+    try {
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function show(req, res) {
+    try {
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function update(req, res) {
+    try {
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function deleteItenerary(req, res) {
+    try {
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function createFlight(req, res) {
+    try {
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function createAccomodation(req, res) {
+    try {
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export {
+    create,
+    index,
+    show,
+    update,
+    deleteItenerary as delete,
+    createFlight,
+    createAccomodation,
+}
