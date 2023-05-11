436f2f1224e3e0d190a33565110f0122f37913ae
Monica Sue <monicasue567@gmail.com>
2023-05-11 18:49:22 -0400

    add routes to itineraries, flights, and accomodations

---
+++ routes/itineraries.js
@@ -0,0 +1,25 @@
+import { Router } from 'express'
+import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
+import * as itinerariesCtrl from '../controllers/itineraries.js'
+
+const router = Router()
+
+/*---------- Public Routes ----------*/
+
+
+/*---------- Protected Routes ----------*/
+router.use(decodeUserFromToken)
+router.get('/', checkAuth, itinerariesCtrl.index)
+router.get('/:itineraryId', checkAuth, itinerariesCtrl.show)
+router.post('/', checkAuth, itinerariesCtrl.create)
+router.post('/:itineraryId/flights', checkAuth, itinerariesCtrl.createFlight)
+router.post('/:itineraryId/accomodations', checkAuth, itinerariesCtrl.createAccomodation)
+router.put('/:itineraryId', checkAuth, itinerariesCtrl.update)
+router.put('/:itineraryId/flights/:flightId', checkAuth, itinerariesCtrl.updateFlight)
+router.put('/:itineraryId/accomodations/:accomodationId', checkAuth, itinerariesCtrl.updateAccomodation)
+router.delete('/:itineraryId', checkAuth, itinerariesCtrl.delete)
+router.delete('/:itineraryId/flights/:flightId', checkAuth, itinerariesCtrl.deleteFlight)
+router.delete('/:itineraryId/accomodations/:accomodationId', checkAuth, itinerariesCtrl.deleteAccomodation)
+
+
+export { router }
--- server.js
+++ server.js
@@ -11,6 +11,7 @@ import './config/database.js'
 // import routes
 import { router as profilesRouter } from './routes/profiles.js'
 import { router as authRouter } from './routes/auth.js'
+import { router as itinerariesRouter } from './routes/itineraries.js'
 
 // create the express app
 const app = express()
@@ -24,6 +25,7 @@ app.use(formData.parse())
 // mount imported routes
 app.use('/api/profiles', profilesRouter)
 app.use('/api/auth', authRouter)
+app.use('/api/itineraries', itinerariesRouter)
 
 // handle 404 errors
 app.use(function (req, res, next) {
