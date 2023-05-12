80d1624c74ff7d58630190eb6efac5a711678b61
CurrentlyBobe <walshbobby5@gmail.com>
2023-05-11 21:40:53 -0500

    format adjust

--- controllers/itineraries.js
+++ controllers/itineraries.js
@@ -1,148 +1,151 @@
 import { Profile } from "../models/profile.js";
 import { Itinerary } from "../models/itinerary.js";
 
-
 async function create(req, res) {
   try {
-    req.body.owner= req.user.profile
-    const itinerary= await Itinerary.create(req.body)
-    res.status(201).json(itinerary)
+    req.body.owner = req.user.profile;
+    const itinerary = await Itinerary.create(req.body);
+    res.status(201).json(itinerary);
   } catch (error) {
-    console.log(error)
-    res.status(500).json(error)
+    console.log(error);
+    res.status(500).json(error);
   }
 }
 
 async function index(req, res) {
   try {
-    const itinerary= await Itinerary.find({owner: req.user.profile})
-    // .sort SORT BY START DATE 
-    res.status(201).json(itinerary)
+    const itinerary = await Itinerary.find({ owner: req.user.profile });
+    // .sort SORT BY START DATE
+    res.status(201).json(itinerary);
   } catch (error) {
-    console.log(error)
-    res.status(500).json(error)
+    console.log(error);
+    res.status(500).json(error);
   }
 }
 
 async function show(req, res) {
   try {
-    const itinerary= await Itinerary.findById(req.params.itineraryId)
-    res.status(200).json(itinerary)
+    const itinerary = await Itinerary.findById(req.params.itineraryId);
+    res.status(200).json(itinerary);
   } catch (error) {
-    console.log(error)
-    res.status(500).json(error)
+    console.log(error);
+    res.status(500).json(error);
   }
 }
 
 async function update(req, res) {
   try {
-    const itinerary= await Itinerary.findByIdAndUpdate(req.params.itineraryId, req.body, {new: true})
-    res.status(200).json(itinerary)
+    const itinerary = await Itinerary.findByIdAndUpdate(
+      req.params.itineraryId,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(itinerary);
   } catch (error) {
-    console.log(error)
-    res.status(500).json(error)
+    console.log(error);
+    res.status(500).json(error);
   }
 }
 
 async function deleteItinerary(req, res) {
   try {
-    const itinerary= await Itinerary.findByIdAndDelete(req.params.itineraryId)
-    res.status(200).json(itinerary)
+    const itinerary = await Itinerary.findByIdAndDelete(req.params.itineraryId);
+    res.status(200).json(itinerary);
   } catch (error) {
-    console.log(error)
-    res.status(500).json(error)
+    console.log(error);
+    res.status(500).json(error);
   }
 }
 
 async function createFlight(req, res) {
   try {
-    const itinerary= await Itinerary.findById(req.params.itineraryId)
-    itinerary.flights.push(req.body)
-    await itinerary.save()
-    const flights= itinerary.flights[itinerary.flights.length -1]
-    res.status(201).json(flights)
+    const itinerary = await Itinerary.findById(req.params.itineraryId);
+    itinerary.flights.push(req.body);
+    await itinerary.save();
+    const flights = itinerary.flights[itinerary.flights.length - 1];
+    res.status(201).json(flights);
   } catch (error) {
-    console.log(error)
-    res.status(500).json(error)
+    console.log(error);
+    res.status(500).json(error);
   }
 }
 
 async function updateFlight(req, res) {
   try {
-    const itinerary= await Itinerary.findById(req.params.itineraryId)
-    const flight= itinerary.flights.id(req.params.flightId)
-    flight.set(req.body)
-    await itinerary.save()
-    res.status(201).json(itinerary)
+    const itinerary = await Itinerary.findById(req.params.itineraryId);
+    const flight = itinerary.flights.id(req.params.flightId);
+    flight.set(req.body);
+    await itinerary.save();
+    res.status(201).json(itinerary);
   } catch (error) {
-    console.log(error)
-    res.status(500).json(error)
+    console.log(error);
+    res.status(500).json(error);
   }
 }
 
 async function updateAccommodation(req, res) {
   try {
-    const itinerary= await Itinerary.findById(req.params.itineraryId)
-    const accommodation= itinerary.accommodations.id(req.params.accommodationId)
-    accommodation.set(req.body)
-    await itinerary.save()
-    res.status(201).json(itinerary)
+    const itinerary = await Itinerary.findById(req.params.itineraryId);
+    const accommodation = itinerary.accommodations.id(
+      req.params.accommodationId
+    );
+    accommodation.set(req.body);
+    await itinerary.save();
+    res.status(201).json(itinerary);
   } catch (error) {
-    console.log(error)
-    res.status(500).json(error)
+    console.log(error);
+    res.status(500).json(error);
   }
 }
 
 async function createAccommodation(req, res) {
   try {
-    const itinerary= await Itinerary.findById(req.params.itineraryId)
-    itinerary.accommodations.push(req.body)
-    await itinerary.save()
-    const accommodations= itinerary.accommodations[itinerary.accommodations.length -1]
-    res.status(201).json(accommodations)
+    const itinerary = await Itinerary.findById(req.params.itineraryId);
+    itinerary.accommodations.push(req.body);
+    await itinerary.save();
+    const accommodations =
+      itinerary.accommodations[itinerary.accommodations.length - 1];
+    res.status(201).json(accommodations);
   } catch (error) {
-    console.log(error)
-    res.status(500).json(error)
+    console.log(error);
+    res.status(500).json(error);
   }
 }
 
 async function deleteFlight(req, res) {
   try {
-    const itinerary= await Itinerary.findById(req.params.itineraryId)
-    itinerary.flights.remove({_id: req.params.flightId})
-    await itinerary.save()
-    res.status(201).json(itinerary)
+    const itinerary = await Itinerary.findById(req.params.itineraryId);
+    itinerary.flights.remove({ _id: req.params.flightId });
+    await itinerary.save();
+    res.status(201).json(itinerary);
   } catch (error) {
-    console.log(error)
-    res.status(500).json(error)
+    console.log(error);
+    res.status(500).json(error);
   }
 }
 
 async function deleteAccommodation(req, res) {
   try {
-    const itinerary= await Itinerary.findById(req.params.itineraryId)
-    itinerary.accommodations.remove({_id: req.params.accommodationId})
-    await itinerary.save()
-    res.status(201).json(itinerary)
+    const itinerary = await Itinerary.findById(req.params.itineraryId);
+    itinerary.accommodations.remove({ _id: req.params.accommodationId });
+    await itinerary.save();
+    res.status(201).json(itinerary);
   } catch (error) {
-    console.log(error)
-    res.status(500).json(error)
+    console.log(error);
+    res.status(500).json(error);
   }
 }
 
-
-
-
 export {
-    create,
-    index,
-    show,
-    update,
-    deleteItinerary as delete,
-    createFlight,
-    createAccommodation,
-    updateFlight,
-    updateAccommodation,
-    deleteFlight,
-    deleteAccommodation,
-}
+  create,
+  index,
+  show,
+  update,
+  deleteItinerary as delete,
+  createFlight,
+  createAccommodation,
+  updateFlight,
+  updateAccommodation,
+  deleteFlight,
+  deleteAccommodation,
+};
--- models/itinerary.js
+++ models/itinerary.js
@@ -1,49 +1,42 @@
 import mongoose from "mongoose";
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
+const flightSchema = new Schema({
+  airline: { type: String, required: true },
+  roundtrip: Boolean,
+  departureAirport: { type: String, required: true },
+  departureDate: Date,
+  arrivalAirport: String,
+  arrivalDate: Date,
+  recordLocator: String,
+  cost: Number,
+});
 
-const flightSchema = new Schema(
-  {
-    airline: {type: String, required: true},
-    roundtrip: Boolean,
-    departureAirport: {type: String, required: true},
-    departureDate: Date,
-    arrivalAirport: String, 
-    arrivalDate: Date,
-    recordLocator: String,
-    cost: Number
-  }
-)
+const accommodationSchema = new Schema({
+  type: { type: String, enum: ["Hotel", "Cruise", "Airbnb", "Other"] },
+  name: String,
+  checkInDate: Date,
+  checkOutDate: Date,
+  address: String,
+  website: String,
+  cost: Number,
+});
 
-const accommodationSchema = new Schema(
-  {
-    type: {type: String, enum:["Hotel", "Cruise", "Airbnb", "Other"]},
-    name: String,
-    checkInDate: Date,
-    checkOutDate: Date,
-    address: String,
-    website: String,
-    cost: Number
-  }
-)
-
-const itinerarySchema = new Schema ({
-
-  title: {type: String, required: true},
+const itinerarySchema = new Schema({
+  title: { type: String, required: true },
   // photo: String,
   budget: Number,
   cost: Number,
   startDate: Date,
   endDate: Date,
-  city: {type: String, required: true},
-  country: {type: String, required: true},
+  city: { type: String, required: true },
+  country: { type: String, required: true },
   flights: [flightSchema],
   accommodations: [accommodationSchema],
-  owner: {type: Schema.Types.ObjectId, ref: "Profile"}
-})
+  owner: { type: Schema.Types.ObjectId, ref: "Profile" },
+});
 
+const Itinerary = mongoose.model("Itinerary", itinerarySchema);
 
-const Itinerary = mongoose.model('Itinerary', itinerarySchema)
-
-export {Itinerary}
+export { Itinerary };
