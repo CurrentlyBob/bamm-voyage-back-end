965528d03456e95d7f30295fc057946f8a8a96c7
Andrea <andrearewards@hotmail.com>
2023-05-11 20:05:05 -0400

    fix indentation on controller functions

--- controllers/itineraries.js
+++ controllers/itineraries.js
@@ -3,91 +3,91 @@ import { Itinerary } from "../models/itinerary.js";
 
 
 async function create(req, res) {
-    try {
-        
-    } catch (error) {
-        console.log(error)
-    }
+  try {
+
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 async function index(req, res) {
-    try {
-        
-    } catch (error) {
-        console.log(error)
-    }
+  try {
+
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 async function show(req, res) {
-    try {
-        
-    } catch (error) {
-        console.log(error)
-    }
+  try {
+
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 async function update(req, res) {
-    try {
-        
-    } catch (error) {
-        console.log(error)
-    }
+  try {
+
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 async function deleteItinerary(req, res) {
-    try {
-        
-    } catch (error) {
-        console.log(error)
-    }
+  try {
+
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 async function createFlight(req, res) {
-    try {
-        
-    } catch (error) {
-        console.log(error)
-    }
+  try {
+
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 async function updateFlight(req, res) {
-    try {
-        
-    } catch (error) {
-        console.log(error)
-    }
+  try {
+
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 async function updateAccommodation(req, res) {
-    try {
-        
-    } catch (error) {
-        console.log(error)
-    }
+  try {
+
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 async function createAccommodation(req, res) {
-    try {
-        
-    } catch (error) {
-        console.log(error)
-    }
+  try {
+
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 async function deleteFlight(req, res) {
-    try {
-        
-    } catch (error) {
-        console.log(error)
-    }
+  try {
+
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 async function deleteAccommodation(req, res) {
-    try {
-        
-    } catch (error) {
-        console.log(error)
-    }
+  try {
+
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 
