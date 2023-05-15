0a50702ed442410d10cf0f3f50c3d5aa4633c90a
melvinf21 <melvinfontanez@gmail.com>
2023-05-15 10:42:12 -0400

    adds activity schema and controller functions

--- routes/itineraries.js
+++ routes/itineraries.js
@@ -12,14 +12,17 @@ router.use(decodeUserFromToken)
 router.get('/', checkAuth, itinerariesCtrl.index)
 router.get('/:itineraryId', checkAuth, itinerariesCtrl.show)
 router.post('/', checkAuth, itinerariesCtrl.create)
+router.post('/:itineraryId/activities', checkAuth, itinerariesCtrl.createActivity)
 router.post('/:itineraryId/flights', checkAuth, itinerariesCtrl.createFlight)
 router.post('/:itineraryId/accommodations', checkAuth, itinerariesCtrl.createAccommodation)
 router.put('/:itineraryId', checkAuth, itinerariesCtrl.update)
 router.put('/:itineraryId/flights/:flightId', checkAuth, itinerariesCtrl.updateFlight)
 router.put('/:itineraryId/accommodations/:accommodationId', checkAuth, itinerariesCtrl.updateAccommodation)
+router.put('/:itineraryId/activities/:activityId', checkAuth, itinerariesCtrl.updateActivity)
 router.delete('/:itineraryId', checkAuth, itinerariesCtrl.delete)
 router.delete('/:itineraryId/flights/:flightId', checkAuth, itinerariesCtrl.deleteFlight)
 router.delete('/:itineraryId/accommodations/:accommodationId', checkAuth, itinerariesCtrl.deleteAccommodation)
+router.delete('/:itineraryId/activities/:activityId', checkAuth, itinerariesCtrl.deleteActivity)
 
 
 export { router }
